a13e4a89c1817e7beefe5796013d3c2d0d34cafc
kranthikiran01 <guduru.kranthikiran@gmail.com>
2019-03-03 19:13:21 +0530

    added null and empty util in slice

--- sliceutil/sliceutil.go
+++ sliceutil/sliceutil.go
@@ -2,13 +2,14 @@ package sliceutil
 
 import (
 	"reflect"
+	"errors"
 )
 
 /*
 	Usage: find(a, func(i interface{}) bool { return i == 5 })
-	finds index of a value in a given slice
+	returns index of a value in a given slice
 	Accepts:
-		- slice : a generic slice of an array
+		- slice : slice of an array
 		  f     : filter function for finding the element
 */
 func Find(slice interface{}, f func(interface{}) bool) int {
@@ -24,11 +25,71 @@ func Find(slice interface{}, f func(interface{}) bool) int {
     return -1
 }
 
-func Contains(slice interface{}, value interface{}) bool {
+/*
+	Usage: contains(a, 34)
+	returns index of a value in a given slice
+	Accepts:
+		- slice : slice of an array
+		  value     : value of matching type of element in slice
+*/
+func Contains(slice interface{}, value interface{}) (bool,error) {
+	if(IsEmpty(slice) || isNil(slice)) {
+		return nil, errors.New("Slice can't be empty")
+	}
 	var index int = Find(slice, func(i interface{}) bool { return i==value })
 	if(index>-1) {
 		return true
 	} else {
 		return false
 	}
+}
+
+/*
+	Usage: IsNil(a)
+	Returns a boolean value of IsNil of a given slice
+	Accepts:
+		- slice  : slice of an array
+
+*/
+func IsNil(slice interface{}) (bool,error) {
+	return slice==nil;
+}
+
+/*
+	Usage: IsNotNil(a)
+	Returns a boolean value of IsNotNil of a given slice
+	Accepts:
+		- slice  : slice of an array
+
+*/
+func IsNotNil(slice interface{}) (bool,error) {
+	var isNil, error = IsNil(slice);
+	return !isNil;
+}
+
+/*
+	Usage: IsEmpty(a)
+	Returns a boolean value of IsEmpty of a given slice
+	Accepts:
+		- slice  : slice of an array
+
+*/
+func IsEmpty(slice interface{}) (bool,error) {
+	if(slice==nil) {
+		return errors.New("Slice passed is nil.")
+	}
+	return len(slice)==0
+}
+
+
+/*
+	Usage: IsNotEmpty(a)
+	Returns a boolean value of IsNotEmpty of a given slice
+	Accepts:
+		- slice  : slice of an array
+
+*/
+func IsNotEmpty(slice interface{}) (bool,error) {
+	var isEmpty,error = IsEmpty(slice);
+	return !isEmpty;
 }
