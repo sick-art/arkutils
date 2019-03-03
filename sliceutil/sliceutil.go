78b12b9254d4a1405df4a04c89a5bfb41762e1dc
kranthikiran01 <guduru.kranthikiran@gmail.com>
2019-03-03 20:45:50 +0530

    Sliceutil - more functions added

--- sliceutil/sliceutil.go
+++ sliceutil/sliceutil.go
@@ -1,6 +1,7 @@
 package sliceutil
 
 import (
+	"fmt"
 	"reflect"
 	"errors"
 )
@@ -33,14 +34,20 @@ func Find(slice interface{}, f func(interface{}) bool) int {
 		  value     : value of matching type of element in slice
 */
 func Contains(slice interface{}, value interface{}) (bool,error) {
-	if(IsEmpty(slice) || isNil(slice)) {
-		return nil, errors.New("Slice can't be empty")
+	empty,err_empty := IsEmpty(slice)
+	isNil,err_isNil := IsNil(slice)
+	if( empty==true) {
+		return false, errors.New(fmt.Sprintf("Slice can't be empty %s",err_empty))
 	}
+	if( isNil==true) {
+		return false, errors.New(fmt.Sprintf("Slice can't be nil %s",err_isNil))
+	}
+
 	var index int = Find(slice, func(i interface{}) bool { return i==value })
 	if(index>-1) {
-		return true
+		return true, nil
 	} else {
-		return false
+		return false, nil
 	}
 }
 
@@ -52,7 +59,7 @@ func Contains(slice interface{}, value interface{}) (bool,error) {
 
 */
 func IsNil(slice interface{}) (bool,error) {
-	return slice==nil;
+	return (slice==nil),nil;
 }
 
 /*
@@ -63,8 +70,7 @@ func IsNil(slice interface{}) (bool,error) {
 
 */
 func IsNotNil(slice interface{}) (bool,error) {
-	var isNil, error = IsNil(slice);
-	return !isNil;
+	return (!(slice==nil)),nil;
 }
 
 /*
@@ -76,9 +82,9 @@ func IsNotNil(slice interface{}) (bool,error) {
 */
 func IsEmpty(slice interface{}) (bool,error) {
 	if(slice==nil) {
-		return errors.New("Slice passed is nil.")
+		return false, errors.New("Slice can't be nil")
 	}
-	return len(slice)==0
+	return (reflect.ValueOf(slice).Len()==0),nil
 }
 
 
@@ -90,6 +96,54 @@ func IsEmpty(slice interface{}) (bool,error) {
 
 */
 func IsNotEmpty(slice interface{}) (bool,error) {
-	var isEmpty,error = IsEmpty(slice);
-	return !isEmpty;
+	return (!(reflect.ValueOf(slice).Len()==0)),nil;
+}
+
+
+/*
+	Usage: IsEqual(a)
+	Returns a boolean value of IsEqual for two slices
+	Accepts:
+		- slice  : slice of an array
+		  anotherSlice : slice of an array
+
+*/
+func IsEqual(slice interface{}, anotherSlice interface{}) (bool) {
+	return reflect.DeepEqual(slice,anotherSlice)
+}
+
+/*
+	Usage: IsSameLength(a)
+	Returns a boolean value of IsSameLength for two slices
+	Accepts:
+		- slice        : slice of an array
+		  anotherSlice : slice of an array
+*/
+func IsSameLength(slice interface{}, anotherSlice interface{}) (bool) {
+	return reflect.ValueOf(slice).Len()==reflect.ValueOf(anotherSlice).Len()
+}
+
+/*
+	Usage: IsSameType(a)
+	Returns a boolean value of IsSameType for two slices
+	Accepts:
+		- slice        : slice of an array
+		  anotherSlice : slice of an array
+*/
+func IsSameType(slice interface{}, anotherSlice interface{}) (bool) {
+	return reflect.TypeOf(slice)==reflect.TypeOf(anotherSlice)
+}
+
+func LastIndexOf(slice interface{}, valueToFind interface{}, startIndex int) (int) {
+	if(slice==nil){
+		return -1
+	}
+	if( startIndex < 0 ){
+		return -1
+	} else if( startIndex >= reflect.ValueOf(slice).Len()){
+		startIndex = reflect.ValueOf(slice).Len()-1
+	}
+	if(valueToFind==nil){
+		
+	}
 }
